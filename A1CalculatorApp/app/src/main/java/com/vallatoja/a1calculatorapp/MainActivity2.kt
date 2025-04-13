a5ed94a595fc172aee6e0dd057f495a7c5f05599
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2025-04-13 22:45:12 +0400

    Button to go to activity 1 to 2

--- A1CalculatorApp/app/src/main/java/com/vallatoja/a1calculatorapp/MainActivity2.kt
+++ A1CalculatorApp/app/src/main/java/com/vallatoja/a1calculatorapp/MainActivity2.kt
@@ -1,28 +1,20 @@
 package com.vallatoja.a1calculatorapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        // func for button start (activity 1 to 2)
-        val secondActivityButton = findViewById<Button>(R.id.buttonstart)
-        secondActivityButton.setOnClickListener {
-            val Intent = Intent(this,MainActivity2::class.java) // pass the class as the 2nd argument
-            startActivity(Intent)
         }
     }
 }
